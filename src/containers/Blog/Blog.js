647cb7f465ec8fcccb395a3b5a3b3bb6df501d67
Amanda Tree <amandasimonds9@gmail.com>
2020-05-31 17:16:23 -0600

    added get, post delete requests, baseurl, and axios configuration logs to index.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,34 +9,58 @@ import './Blog.css';
 class Blog extends Component {
 
     state={
-        posts:[]
+        posts:[],
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios.get("/posts")
             .then(response => {
+                //use slice to only get posts 1-4
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        //distribute property of post
+                        ...post,
+                        //add new property
+                        author: "Max"
+                    }
+                })
                 //we call setstate inside the then block, to prevent js running the rest of the code before getting the data
-                this.setState({posts: response.data});
-                console.log(response);
+                this.setState({posts: updatedPosts});
+                // console.log(response);
+            })
+            .catch(error => {
+                // set the state to error so that it shows a something went wrong" message
+                this.setState({error: true})
             })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
-
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
-        });
-
+        let posts = <p style={{textAlign:"center"}}>Something went wrong</p>
+        if (!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    />
+            });
+        }
+        
         return (
             <div>
                 <section className="Posts">
                     {posts}
-                    <Post />
-                    <Post />
-                    <Post />
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+
+//logs request configuration
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //Edit request config, if you want to such as adding headers
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(request => {
+    console.log(request);
+    //Edit request config, if you want to such as adding headers
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
