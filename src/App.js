3b940b2d76b7aa7ffbed8872a38a8682df889944
Amanda Tree <amandasimonds9@gmail.com>
2020-06-07 14:59:03 -0600

    added browserouter basename comment as a reminder that a basename property is needed when hosting your react app on a domain

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+
+      //if you are using a domain name, you have to set the basename url for the routes to work
+      // <BrowserRouter basename ="/my-app">
+
       <BrowserRouter>
         <div className="App">
          <Blog />
